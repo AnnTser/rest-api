62f347f2bc322dd07886e3e1a27bfc813c8ed7b0
AnnTs <atserkovnikova.qa@gmail.com>
2023-11-29 08:02:34 +0300

    added specs

--- src/test/java/tests/LoginExtendedTests.java
+++ src/test/java/tests/LoginExtendedTests.java
@@ -3,6 +3,7 @@ package tests;
 import io.qameta.allure.restassured.AllureRestAssured;
 import models.lombok.LoginBodyLombokModel;
 import models.lombok.LoginResponseLombokModel;
+import models.lombok.MissingPasswordModel;
 import models.pojo.LoginBodyPojoModel;
 import models.pojo.LoginResponsePojoModel;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,7 @@ import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static specs.LoginSpec.*;
 
 public class LoginExtendedTests {
 
@@ -36,12 +38,12 @@ public class LoginExtendedTests {
                 .body("token", is("QpwL5tke4Pnpja7X4"));
     }
 
-@Test
+    @Test
     void successfulLoginWithPojoTest() {
-    LoginBodyPojoModel authBody = new LoginBodyPojoModel();
-    authBody.setEmail("eve.holt@reqres.in");
-    authBody.setPassword("cityslicka");
-    LoginResponsePojoModel response = given()
+        LoginBodyPojoModel authBody = new LoginBodyPojoModel();
+        authBody.setEmail("eve.holt@reqres.in");
+        authBody.setPassword("cityslicka");
+        LoginResponsePojoModel response = given()
                 .log().uri()
                 .log().method()
                 .log().body()
@@ -54,15 +56,15 @@ public class LoginExtendedTests {
                 .log().body()
                 .statusCode(200)
                 .extract().as(LoginResponsePojoModel.class);
-    assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
+        assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
     }
 
     @Test
     void successfulLoginWithLombokTest() {
-    LoginBodyLombokModel authBody = new LoginBodyLombokModel();
-    authBody.setEmail("eve.holt@reqres.in");
-    authBody.setPassword("cityslicka");
-    LoginResponseLombokModel response = given()
+        LoginBodyLombokModel authBody = new LoginBodyLombokModel();
+        authBody.setEmail("eve.holt@reqres.in");
+        authBody.setPassword("cityslicka");
+        LoginResponseLombokModel response = given()
                 .log().uri()
                 .log().method()
                 .log().body()
@@ -75,17 +77,17 @@ public class LoginExtendedTests {
                 .log().body()
                 .statusCode(200)
                 .extract().as(LoginResponseLombokModel.class);
-    assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
+        assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
     }
 
     @Test
     void successfulLoginWithAllureTest() {
-    LoginBodyLombokModel authBody = new LoginBodyLombokModel();
-    authBody.setEmail("eve.holt@reqres.in");
-    authBody.setPassword("cityslicka");
+        LoginBodyLombokModel authBody = new LoginBodyLombokModel();
+        authBody.setEmail("eve.holt@reqres.in");
+        authBody.setPassword("cityslicka");
 
-    LoginResponseLombokModel response = given()
-            .filter(new AllureRestAssured())
+        LoginResponseLombokModel response = given()
+                .filter(new AllureRestAssured())
                 .log().uri()
                 .log().method()
                 .log().body()
@@ -98,17 +100,17 @@ public class LoginExtendedTests {
                 .log().body()
                 .statusCode(200)
                 .extract().as(LoginResponseLombokModel.class);
-    assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
+        assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
     }
 
     @Test
     void successfulLoginWithCustomAllureTest() {
-    LoginBodyLombokModel authBody = new LoginBodyLombokModel();
-    authBody.setEmail("eve.holt@reqres.in");
-    authBody.setPassword("cityslicka");
+        LoginBodyLombokModel authBody = new LoginBodyLombokModel();
+        authBody.setEmail("eve.holt@reqres.in");
+        authBody.setPassword("cityslicka");
 
-    LoginResponseLombokModel response = given()
-            .filter(withCustomTemplates())
+        LoginResponseLombokModel response = given()
+                .filter(withCustomTemplates())
                 .log().uri()
                 .log().method()
                 .log().body()
@@ -121,67 +123,81 @@ public class LoginExtendedTests {
                 .log().body()
                 .statusCode(200)
                 .extract().as(LoginResponseLombokModel.class);
-    assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
+        assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
     }
 
     @Test
     void successfulLoginWithAllureStepsTest() {
-    LoginBodyLombokModel authBody = new LoginBodyLombokModel();
-    authBody.setEmail("eve.holt@reqres.in");
-    authBody.setPassword("cityslicka");
+        LoginBodyLombokModel authBody = new LoginBodyLombokModel();
+        authBody.setEmail("eve.holt@reqres.in");
+        authBody.setPassword("cityslicka");
 
 
-        LoginResponseLombokModel response =  step ("Make login request", () ->
+        LoginResponseLombokModel response = step("Make login request", () ->
 
-        given()
-            .filter(withCustomTemplates())
-                .log().uri()
-                .log().method()
-                .log().body()
-                .body(authBody)
-                .contentType(JSON)
-                .when()
-                .post("https://reqres.in/api/login")
-                .then()
-                .log().status()
-                .log().body()
-                .statusCode(200)
-                .extract().as(LoginResponseLombokModel.class));
+                given()
+                        .filter(withCustomTemplates())
+                        .log().uri()
+                        .log().method()
+                        .log().body()
+                        .body(authBody)
+                        .contentType(JSON)
+                        .when()
+                        .post("https://reqres.in/api/login")
+                        .then()
+                        .log().status()
+                        .log().body()
+                        .statusCode(200)
+                        .extract().as(LoginResponseLombokModel.class));
 
 
-        step ("Verify response", () ->
-    assertEquals("QpwL5tke4Pnpja7X4", response.getToken()));
+        step("Verify response", () ->
+                assertEquals("QpwL5tke4Pnpja7X4", response.getToken()));
+
     }
 
+    @Test
+    void successfulLoginWithSpecsTest() {
+        LoginBodyLombokModel authBody = new LoginBodyLombokModel();
+        authBody.setEmail("eve.holt@reqres.in");
+        authBody.setPassword("cityslicka");
+
+        LoginResponseLombokModel response = step("Make login request", () ->
+                given(loginRequestSpec)
+                        .body(authBody)
+                        .when()
+                        .post()
+                        .then()
+                        .spec(loginResponseSpec)
+                        .extract().as(LoginResponseLombokModel.class));
+        step("Verify response", () ->
+                assertEquals("QpwL5tke4Pnpja7X4", response.getToken()));
+    }
+
+
+    @Test
+    void missingPasswordTest() {
+        LoginBodyLombokModel authBody = new LoginBodyLombokModel();
+        authBody.setEmail("eve.holt@reqres.in");
+
+
+        MissingPasswordModel response = step("Make login request", () ->
+                given(loginRequestSpec)
+                        .body(authBody)
+                        .when()
+                        .post()
+                        .then()
+                        .spec(missingPasswordSpec)
+                        .extract().as(MissingPasswordModel.class));
+        step("Verify response", () ->
+                assertEquals("Missing password", response.getError()));
+
+    }
 
 
 }
 
 
-
-
-
-
-
-//    @Test
-//    void missingPasswordTest() {
-//        String authBody = "{ \"email\": \"eve.holt@reqres.in\" }"; // BAD PRACTICE
-//
-//        given()
-//                .log().uri()
-//                .log().method()
-//                .log().body()
-//                .body(authBody)
-//                .contentType(JSON)
-//                .when()
-//                .post("https://reqres.in/api/login")
-//                .then()
-//                .log().status()
-//                .log().body()
-//                .statusCode(400)
-//                .body("error", is("Missing password"));
-//    }
-//
 //
 //    @Test
 //    void negative400LoginTest() {
